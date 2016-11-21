7121b7935b6427e8da59233788ce9386abb6689f
Tomasz Janicki <tomaszjanicki@ciber.com>
2016-11-21 15:39:35 +0100

    Add project files.

---
+++ Demo.Api/App_Start/WebApiConfig.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Http;
+
+namespace Demo.Api
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute("DefaultApi", "{controller}/{id}", new { id = RouteParameter.Optional });
+        }
+    }
+}
