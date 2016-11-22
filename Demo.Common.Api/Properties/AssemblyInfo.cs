d4d9ba79d62ed851efd10c6f60bde9644c4fbd15
Tomasz Janicki <tomaszjanicki@ciber.com>
2016-11-22 12:54:51 +0100

    add new project

--- Demo.Common.Api/Properties/AssemblyInfo.cs
+++ Demo.Common.Api/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Demo.Dtos")]
+[assembly: AssemblyTitle("Demo.Common.Api")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Demo.Dtos")]
+[assembly: AssemblyProduct("Demo.Common.Api")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ee7282e6-c57d-45c9-8855-23e055c5130e")]
+[assembly: Guid("d49321a6-12ec-4b32-8b8b-3e17cd800bb4")]
 
 // Version information for an assembly consists of the following four values:
 //
